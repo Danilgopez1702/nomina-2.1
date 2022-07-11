698ff94115e48148092df1033b88f4c37cc1fb41
Cami <polocaco@hotmail.com>
2022-07-11 17:25:21 -0500

    Formulario de nuevo empleado vista

--- incluides/modal/empleados.php
+++ incluides/modal/empleados.php
@@ -8,48 +8,62 @@
             <div class="container"></div>
                 <div class="modal-body">
                     <form class="forms-sample" method='post' action='' enctype="multipart/form-data">
-                      <p class="card-description"> Informacion Personal </p>
-                      <div class="row">
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Nombre(s)</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Apellido Paterno</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Apellido Materno</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">No. de cuenta</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
+                      <div class="form-group row">
+                      <div class="col-md-6" style="padding-left: 100px;">
+                       <div class="col-md-6">
+                        <div class="form-group row">
+                        <input type="file" id="files" name="files[]">
+                        <output id="list"></output>
                       </div>
-                      <br>
-                      <div class="row">
-                        <div class="col-md-6">
+                      </div> 
+                      </div>
+                      <div class="col-md-6">
+                        <p class="card-description" align="center"> Informacion Personal </p>
+                        <div class="row-md-6">
                           <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Departamento</label>
+                            <label class="col-sm-2 col-form-label">ID de empleado</label>
                             <div class="col-sm-9">
-                              <select class="form-control" id="combo_departamento">
+                              <input type="text" class="form-control" id="id_empleado" name="id_empleado"  />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Nombre(s)</label>
+                            <div class="col-sm-9">
+                              <input type="text" class="form-control" id="nombre" name="nombre" />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Apellido Paterno</label>
+                            <div class="col-sm-9">
+                              <input type="text" class="form-control" id="ap" name="ap" />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Apellido Materno</label>
+                            <div class="col-sm-9">
+                              <input type="text" class="form-control" id="am" name="am" />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">No. de cuenta</label>
+                            <div class="col-sm-9">
+                              <input type="text" class="form-control" id="num_cuenta" name="num_cuenta" />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Departamento</label>
+                            <div class="col-sm-9">
+                              <select class="form-control" id="dep" name="dep">
                                 <?php
                                     $query = mysqli_query($conexion, "SELECT * FROM departamento");
                                     $result = mysqli_num_rows($query);
@@ -65,11 +79,11 @@
                             </div>
                           </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="row-md-6">
                           <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Turno</label>
+                            <label class="col-sm-2 col-form-label">Turno</label>
                             <div class="col-sm-9">
-                            <select class="form-control" id="combo_turno">
+                            <select class="form-control" id="turno" name="turno">
                                 <?php
                                     $query = mysqli_query($conexion, "SELECT * FROM departamento");
                                     $result = mysqli_num_rows($query);
@@ -85,97 +99,48 @@
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-md-6">
+                        <div class="row-md-6">
                           <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Sueldo Mensual</label>
+                            <label class="col-sm-2 col-form-label">Sueldo Mensual</label>
                             <div class="col-sm-9">
-                              <input type="text" class="form-control" />
+                              <input type="text" class="form-control" id="sm" name="sm" />
                             </div>
                           </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="row-md-6">
                           <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Sueldo Diario</label>
+                            <label class="col-sm-2 col-form-label">Sueldo Diario</label>
                             <div class="col-sm-9">
-                              <input type="text" class="form-control" />
+                              <input type="text" class="form-control" id="sd" name="sd" />
                             </div>
                           </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="row-md-6">
                           <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Fecha de entrada</label>
+                            <label class="col-sm-2 col-form-label">Fecha de ingreso</label>
                             <div class="col-sm-9">
-                              <input class="form-control" placeholder="dd/mm/yyyy" />
+                              <input type="date" class="form-control" placeholder="dd/mm/aaaa" id="fecha" name="fecha" />
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <p class="card-description"> Address </p>
-                      <div class="row">
-                        <div class="col-md-6">
+                        <div class="row-md-6">
                           <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Address 1</label>
+                            <label class="col-sm-2 col-form-label">Frecuencia de pago</label>
                             <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">State</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
+                             <select class="form-control" id="fp" name="fp">
+                                <option value="7">Semanal</option>
+                                <option value="14">Catorcenal</option>
+                              </select> 
                             </div>
                           </div>
                         </div>
                       </div>
-                      <div class="row">
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Address 2</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Postcode</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">City</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Country</label>
-                            <div class="col-sm-9">
-                              <select class="form-control" id="combo_country">
-                                <option>America</option>
-                                <option>Italy</option>
-                                <option>Russia</option>
-                                <option>Britain</option>
-                              </select>
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </form>
                 </div>
             <div class="modal-footer">
-                <a href="#" data-dismiss="modal" class="btn">Close</a>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal" id="close_button">Cerrar</button>
+              <button type="submit" class="btn btn-primary submitBtn" id="save_button">Guardar</button>
             </div>
         </div>
     </div>
@@ -202,28 +167,31 @@
     </div>
 </div>
 <script>
-    $(document).ready(function(){
-        $('#submit').click(function(){
-            var form_data = new FormData();
-            // Read selected files
-            var totalfiles = document.getElementById('files').files.length;
-            for (var index = 0; index < totalfiles; index++) {
-                form_data.append("files[]", document.getElementById('files').files[index]);
-            $('#preview').append("<img src='"+URL.createObjectURL(event.target.files[i])+"'>");
-            }
-            // AJAX request
-            $.ajax({
-                url: 'ajaxUpload.php', 
-                type: 'post',
-                data: form_data,
-                dataType: 'json',
-                contentType: false,
-                processData: false,
-                success: function (response) {
-                    alert("Uploaded SuccessFully");
-                    console.log(response);
-                }
-            });
-        });
-    });
+
+function archivo(evt) {
+var files = evt.target.files; // FileList object
+       
+//Obtenemos la imagen del campo "file". 
+  for (var i = 0, f; f = files[i]; i++) {         
+//Solo admitimos imágenes.
+    if (!f.type.match('image.*')) {
+        continue;
+    }
+       
+var reader = new FileReader();
+           
+reader.onload = (function(theFile) {
+return function(e) {
+//Creamos la imagen.
+document.getElementById("list").innerHTML = ['<img id="foto" class="img-fluid" style="height:500px;width:500px;padding-top:60px;" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+               };
+           })(f);
+ reader.readAsDataURL(f);
+       }
+}
+           
+document.getElementById('files').addEventListener('change', archivo, false);
+
 </script>
+
+
